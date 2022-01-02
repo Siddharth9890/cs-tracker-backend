2a4fa4932915f24593377ed1f2e1cc1fefbfff60
Siddhartha9890 <siddharth.a9890@gmail.com>
2022-01-02 17:11:48 +0530

    cors

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Origin", "https://cs-tracker.pages.dev/");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
